dfdbc9708804751698069bc3d1e96a38df670942
lbenvindo <leticiabenvindoo@gmail.com>
2023-11-02 19:27:26 -0300

    resolução do desafio proposto

---
+++ desafio-1.js
@@ -0,0 +1,24 @@
+let nome = "Sr Cara de Pastel"
+let xp = 5000
+
+if (xp < 1000) {
+    console.log("O heroi de nome" + nome + " está no nível ferro")
+} else if (xp >= 1001 && xp <= 2000) {
+    console.log("O heroi de nome " + nome + " está no nível bronze")
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log("O heroi de nome " + nome + " está no nível prata")
+} else if (xp >= 5001 && xp <= 7000) {
+    console.log("O heroi de nome " + nome + " está no nível ouro")
+} else if (xp >= 7001 && xp <= 8000) {
+    console.log("O heroi de nome " + nome + " está no nível platina")
+} else if (xp >= 8001 && xp <= 9000) {
+    console.log("O heroi de nome " + nome + " está no nível ascendente")
+} else if (xp >= 9001 && xp <= 10000) {
+    console.log("O heroi de nome " + nome + " está no nível imortal")
+} else if (xp >= 10001) {
+    console.log("O heroi de nome " + nome + " está no nível radiante")
+}
+
+
+
+
